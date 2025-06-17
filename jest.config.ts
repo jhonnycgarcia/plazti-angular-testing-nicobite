5d04c6c254fb42d23155fb162885df6679a13c8e
Jhonny <jhonnycgarcia@gmail.com>
2025-06-17 11:15:05 -0500

    Cobertyra de codigo: configuracion y analisis en angular

--- jest.config.ts
+++ jest.config.ts
@@ -2,18 +2,17 @@ import { Config } from 'jest';
 
 const config: Config = {
   moduleNameMapper: {
-    "^@shared/(.*)$": "<rootDir>/src/app/shared/$1",
-    "^@products/(.*)$": "<rootDir>/src/app/products/$1",
-    "^@env/(.*)$": "<rootDir>/src/environments/$1"
+    '^@shared/(.*)$': '<rootDir>/src/app/shared/$1',
+    '^@products/(.*)$': '<rootDir>/src/app/products/$1',
+    '^@env/(.*)$': '<rootDir>/src/environments/$1',
   },
   collectCoverage: true,
-  coverageDirectory: "./coverage",
+  coverageDirectory: './coverage',
   collectCoverageFrom: [
-    "src/app/**/*.ts",
-    "!<rootDir>/node_modules/",
-    "!<rootDir>/test/",
+    'src/app/**/*.ts',
+    '!<rootDir>/node_modules/',
+    '!<rootDir>/test/',
   ],
-  coverageReporters: ["html", "text-summary"],
 };
 
 export default config;
