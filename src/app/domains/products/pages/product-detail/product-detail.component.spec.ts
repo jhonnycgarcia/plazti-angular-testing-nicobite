20ef8f6e8564bb16426f72eff6e4dee01cb48523
Jhonny <jhonnycgarcia@gmail.com>
2025-06-18 21:45:26 -0500

    Pruebas complejas en angular: testing de componentes y servicios

--- src/app/domains/products/pages/product-detail/product-detail.component.spec.ts
+++ src/app/domains/products/pages/product-detail/product-detail.component.spec.ts
@@ -2,28 +2,52 @@ import ProductDetailComponent from './product-detail.component';
 import { ProductService } from '@shared/services/product.service';
 import {
   SpectatorRouting,
+  SpyObject,
+  byTestId,
   createRoutingFactory,
   mockProvider,
 } from '@ngneat/spectator/jest';
 import { generateFakeProduct } from '@shared/models/product.mock';
+import { of } from 'rxjs';
+
 describe('ProductDetailComponent', () => {
   let spectator: SpectatorRouting<ProductDetailComponent>;
+  let productService: SpyObject<ProductService>;
   const mockProduct = generateFakeProduct();
 
   const createComponent = createRoutingFactory({
     component: ProductDetailComponent,
-    providers: [mockProvider(ProductService)],
+    providers: [
+      mockProvider(ProductService, {
+        getOneBySlug: jest.fn().mockReturnValue(of(mockProduct)),
+      }),
+    ],
   });
 
   beforeEach(() => {
     spectator = createComponent({
+      detectChanges: false,
       props: {
         slug: mockProduct.slug,
       },
     });
+
+    spectator.setInput('slug', mockProduct.slug);
+    productService = spectator.inject(ProductService);
   });
 
   it('should create', () => {
+    spectator.detectChanges();
     expect(spectator.component).toBeTruthy();
+  });
+
+  it('should display product cover', () => {
+    // productService.getOneBySlug.mockReturnValue(of(mockProduct));
+
+    spectator.detectChanges();
+
+    const cover = spectator.query<HTMLImageElement>(byTestId('cover'));
+    expect(cover).toBeTruthy();
+    expect(cover?.src).toContain(mockProduct.images[0]);
   });
 });
